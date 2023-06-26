ab8c1e178597bdba08fd960c29234db380476705
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-06-27 04:24:36 +0530

    integrate env

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-application.listen(3000,()=>{
-    console.log('listening on port 3000');
+const app=express();
+
+app.listen(process.env.PORT,()=>{
+    console.log('listening on port', process.env.PORT);
 })
