f134636d057e4698b33e3af0ac529d75d316bf3b
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-06-27 04:34:36 +0530

    mongodb init

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app=express();
+const app = express();
 
-app.listen(process.env.PORT,()=>{
-    console.log('listening on port', process.env.PORT);
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to db");
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port", process.env.PORT);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
