23d5589883cf3ccd4dbd0b9467baf64b3e37bfef
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-06-27 04:47:28 +0530

    user init

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userModel = new Schema({
-  name: {
-    type: String,
-    required: true,
+const userModel = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
   },
-  email:{
-    type:String,
-    required:true
-  },
-});
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("user", userModel);
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const User = require('./models/userModel')
 
 const app = express();
 
