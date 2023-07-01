43a7d0133dd692e7503148db66d67a63ad459cec
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-07-02 00:43:45 +0530

    refactoring

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,19 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
+const fileController = require("../controllers/fileController");
 
 
 router.post("/login", async (req, res) => {
     userController.checkAndUpdateUser(req, res);
 });
+
+router.post("/upload",fileController.upload.single('video'), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    console.log(req.file.path);
+    console.log("uploading...");
+    fileController.uploadToDB(req,res);
+    return res.redirect("/");
+  }),
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -5,72 +5,36 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const fs= require("fs");
-const ffmpeg = require('fluent-ffmpeg');
-const path = require('path');
-const multer = require('multer');
+const fs = require("fs");
+const ffmpeg = require("fluent-ffmpeg");
+const path = require("path");
+const multer = require("multer");
 
 const app = express();
+const connectDB = require("./database/db");
 const userRoute = require("./routes/userRoutes");
 const mediaController = require("./controllers/mediaController");
 const fileController = require("./controllers/fileController");
 const authController = require("./controllers/authController");
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const PORT = process.env.PORT || 3000;
+connectDB();
 
 app.use(cors({ origin: true }));
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-const PORT = process.env.PORT || 3000;
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("connected to db");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
-
-// let currentUser;
-app.use(async (req,res,next) => {
+app.use(async (req, res, next) => {
   console.log("in use");
- req.currentUser= await authController.verifyIdToken(req,res);
- console.log(req.currentUser);
+  req.currentUser = await authController.verifyIdToken(req, res);
+  console.log(req.currentUser);
   next();
   console.log("out use");
-})
-
-
-
-
-
-
-//   home
-app.get("/", (req, res) => {
-  // const filePath = "./files/file_example_MP3_1MG.mp3";
-  // mediaController.streamAudio(req, res,filePath);
-  return res.render('home');
 });
 
-app.post("/upload",fileController.upload.single('video'), (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-  console.log(req.file.path);
-  console.log("uploading...");
-  fileController.uploadToDB(req,res);
-  return res.redirect("/");
-}),
-
-//   user authentication
-app.use("/user/", userRoute);
+app.use("/", userRoute);
 
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
