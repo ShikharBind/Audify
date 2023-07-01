a534f04faf8d552dfdeb6eb79781523e0cbc57ac
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-07-01 20:34:55 +0530

    upload file

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,19 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const fs= require("fs");
 const ffmpeg = require('fluent-ffmpeg');
-
+const path = require('path');
+const multer = require('multer');
 
 const app = express();
 const userRoute = require("./routes/userRoutes");
 const mediaController = require("./controllers/mediaController");
+const fileController = require("./controllers/fileController");
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(cors({ origin: true }));
+app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
 
 app.use(morgan('dev'));
@@ -37,9 +43,17 @@ mongoose
 
 //   home
 app.get("/", (req, res) => {
-  const filePath = "./files/file_example_MP3_1MG.mp3";
-  mediaController.streamAudio(req, res,filePath);
+  // const filePath = "./files/file_example_MP3_1MG.mp3";
+  // mediaController.streamAudio(req, res,filePath);
+  return res.render('home');
 });
+
+app.post("/upload",fileController.upload.single('video'), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+
+  return res.redirect("/");
+})
 
 //   user authentication
 app.use("/user/", userRoute);
