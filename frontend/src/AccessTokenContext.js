156e5644f41a3479434490aa93aeb726b40f18cb
pshrey-29 <shrey29303@gmail.com>
2023-07-05 20:07:22 +0530

    persisting login state

--- frontend/src/AccessTokenContext.js
+++ frontend/src/AccessTokenContext.js
@@ -1,12 +1,31 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const AccessTokenContext = createContext();
 
 export const AccessTokenProvider = ({ children }) => {
   const [accessToken, setAccessToken] = useState(null);
 
+  useEffect(() => {
+    const storedAccessToken = localStorage.getItem('accessToken');
+    if (storedAccessToken) {
+      setAccessToken(storedAccessToken);
+    }
+  }, []);
+
+  const updateAccessToken = (newAccessToken) => {
+    setAccessToken(newAccessToken);
+    localStorage.setItem('accessToken', newAccessToken);
+  };
+
+  const clearAccessToken = () => {
+    setAccessToken(null);
+    localStorage.removeItem('accessToken');
+  };
+
   return (
-    <AccessTokenContext.Provider value={{ accessToken, setAccessToken }}>
+    <AccessTokenContext.Provider
+      value={{ accessToken, updateAccessToken, clearAccessToken }}
+    >
       {children}
     </AccessTokenContext.Provider>
   );
