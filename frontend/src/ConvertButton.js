e7ed0aba5aa7d332fa6f9c43e4c4b0601128454a
pshrey-29 <shrey29303@gmail.com>
2023-07-11 02:01:16 +0530

    audio from URL

--- frontend/src/ConvertButton.js
+++ frontend/src/ConvertButton.js
@@ -5,22 +5,53 @@ const ConvertButton = ({ fileId, isConvertDisabled, setConverted }) => {
   const { accessToken } = useContext(AccessTokenContext);
 
   const handleConvert = () => {
-    if (fileId) {
-      fetch(`http://localhost:4000/convert/${fileId}`, {
+    if (fileId.startsWith("http")) {
+      // Conversion for URL
+      fetch(`http://localhost:4000/convert-url`, {
         headers: {
           Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
         },
         method: "POST",
+        body: JSON.stringify({ videoURL: fileId }),
       })
         .then((response) => {
-          // Handle successful conversion
-          console.log("Conversion successful");
-          setConverted(true); // Update the parent component's state
+          if (response.ok) {
+            // Handle successful conversion
+            console.log("URL Conversion successful");
+            setConverted(true);
+          } else {
+            // Handle conversion error
+            console.error("URL Conversion error:", response.status);
+          }
         })
         .catch((error) => {
-          // Handle conversion error
-          console.error("Conversion error:", error);
+          console.error("URL Conversion error:", error);
         });
+    } else {
+      // Conversion for local file
+      if (fileId) {
+        fetch(`http://localhost:4000/convert/${fileId}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+          method: "POST",
+        })
+          .then((response) => {
+            if (response.ok) {
+              // Handle successful conversion
+              console.log("Conversion successful");
+              setConverted(true); // Update the parent component's state
+            } else {
+              // Handle conversion error
+              console.error("Conversion error:", response.status);
+            }
+          })
+          .catch((error) => {
+            // Handle conversion error
+            console.error("Conversion error:", error);
+          });
+      }
     }
   };
 
