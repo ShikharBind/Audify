9cc587ffc6b60f743d1c9d21f2f9ec39ea86ccbc
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-08-22 20:42:29 +0530

    yt search

--- backend/app.js
+++ backend/app.js
@@ -49,49 +49,80 @@ const getToken = async () => {
 };
 
 // Get an access token
-getToken()
-  .then((accessToken) => {
-    console.log(`Access Token: ${accessToken}`);
+// getToken()
+//   .then((accessToken) => {
+//     console.log(`Access Token: ${accessToken}`);
 
-    const ACCESS_TOKEN = accessToken; // Replace with the access token you obtained
+//     const ACCESS_TOKEN = accessToken; // Replace with the access token you obtained
 
-    // Replace with the playlist ID you want to fetch details for
-    const PLAYLIST_ID = "00PK3kZHUfMcpL6ldpQDHX?si=4e431d9289424063";
-    const getPlaylistDetails = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.spotify.com/v1/playlists/${PLAYLIST_ID}`,
-          {
-            headers: {
-              Authorization: `Bearer ${ACCESS_TOKEN}`,
-            },
-          }
-        );
-        const playlist = response.data;
-        console.log("Playlist Name:", playlist.name);
-        console.log("Total Tracks:", playlist.tracks.total);
+//     // Replace with the playlist ID you want to fetch details for
+//     const PLAYLIST_ID = "00PK3kZHUfMcpL6ldpQDHX?si=4e431d9289424063";
+//     const getPlaylistDetails = async () => {
+//       try {
+//         const response = await axios.get(
+//           `https://api.spotify.com/v1/playlists/${PLAYLIST_ID}`,
+//           {
+//             headers: {
+//               Authorization: `Bearer ${ACCESS_TOKEN}`,
+//             },
+//           }
+//         );
+//         const playlist = response.data;
+//         console.log("Playlist Name:", playlist.name);
+//         console.log("Total Tracks:", playlist.tracks.total);
 
-        // Loop through the tracks and access their details
-        const tracks = [];
-        playlist.tracks.items.forEach((item, index) => {
-          const track = item.track;
-          // console.log(track);
-          // console.log(`${index + 1}. ${track.name} - ${track.artists[0].name}`);
-          tracks.push(`${track.name} ${track.artists[0].name}`);
-        });
-        console.log(tracks);
-      } catch (error) {
-        console.error("Error fetching playlist details:", error);
+//         // Loop through the tracks and access their details
+//         const tracks = [];
+//         playlist.tracks.items.forEach((item, index) => {
+//           const track = item.track;
+//           // console.log(track);
+//           // console.log(`${index + 1}. ${track.name} - ${track.artists[0].name}`);
+//           tracks.push(`${track.name} ${track.artists[0].name}`);
+//         });
+//         console.log(tracks);
+//       } catch (error) {
+//         console.error("Error fetching playlist details:", error);
+//       }
+//     };
+
+//     // Call the function to get playlist details
+//     getPlaylistDetails();
+//     // Make requests to the Spotify API with this access token
+//   })
+//   .catch((error) => {
+//     console.error("Error getting access token:", error);
+//   });
+
+const API_KEY = process.env.YOUTUBE_API_KEY; // Replace with your actual API key
+const searchTerm = "Despacito"; // Replace with your desired search term
+
+const searchYouTube = async () => {
+  try {
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        params: {
+          key: API_KEY,
+          q: searchTerm,
+          part: "snippet",
+          type: "video",
+          maxResults: 5, // Number of results you want
+        },
       }
-    };
+    );
 
-    // Call the function to get playlist details
-    getPlaylistDetails();
-    // Make requests to the Spotify API with this access token
-  })
-  .catch((error) => {
-    console.error("Error getting access token:", error);
-  });
+    const videos = response.data.items;
+    videos.forEach((video, index) => {
+      const youtubeLink = `https://www.youtube.com/watch?v=${video.id.videoId}`;
+      console.log(youtubeLink);
+    });
+  } catch (error) {
+    console.error("Error searching YouTube:", error);
+  }
+};
+
+// Call the function to search YouTube
+searchYouTube();
 
 app.use(cors({ origin: true }));
 app.use(express.urlencoded({ extended: true }));
